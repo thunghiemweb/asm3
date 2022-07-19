e6dfeecd96632ffb81d39533d743470ee0f14666
tu m4800 <tinpc07@gmail.com>
2022-07-19 12:36:42 +0700

    Them tinh nang sap xep theo luong

--- src/trang/TrangBangLuong.js
+++ src/trang/TrangBangLuong.js
@@ -4,9 +4,9 @@ import { useState } from "react";
 
 function TrangBangLuong(props) {
 
-    const [BangLuong, setBangLuong] = useState(props);
+    const [dsnv, setdsnv] = useState(props.dsnv);
 
-    const mang = BangLuong.dsnv.map((e) => {
+    const mang = dsnv.map((e) => {
         return (
             <div key={e.id} className="col-12 col-sm-6 col-md-4 " >
                 <div style={{ padding: "30px" }}>
@@ -14,29 +14,24 @@ function TrangBangLuong(props) {
                     <p style={{ textAlign: "left" }}>Mã nhân viên:{e.id}</p>
                     <p style={{ textAlign: "left" }}>Hệ số lương:{e.salaryScale}</p>
                     <p style={{ textAlign: "left" }}>Số ngày làm thêm:{e.overTime}</p>
-                    <h6 >Lương: {new Intl.NumberFormat('vi-VN', { maximumSignificantDigits: 3 }).format(3000000 * e.salaryScale + 200000 * e.overTime)}</h6>
+                    <h6 >Lương: {new Intl.NumberFormat('vi-VN', { maximumSignificantDigits: 3 }).format(tinhluong(e))}</h6>
                 </div>
             </div>
         );
     });
 
 
-
-    const objectComparisonCallback = (A, B) => {
-        if (A.annualLeave < B.annualLeave) {
-            return -1
-        }
-
-        if (A.annualLeave > B.annualLeave) {
-            return 1
-        }
-        return 0
+    function caodenthap() {
+        alert('Đã sắp xếp lương từ cao đền thấp');
+        setdsnv(dsnv.sort((a, b) => tinhluong(a) - tinhluong(b)));
+    }
+    function thapdencao() {
+        alert('Đã xắp sếp lương từ thấp đến cao');
+        setdsnv(dsnv.sort((a, b) => tinhluong(b) - tinhluong(a)));
     }
 
-    function caodenthap() {
-        alert('cao den thap');
-        console.log(BangLuong);
-        setBangLuong(BangLuong.sort(objectComparisonCallback));
+    function tinhluong(e) {
+        return 3000000 * e.salaryScale + 200000 * e.overTime;
     }
 
 
@@ -47,15 +42,14 @@ function TrangBangLuong(props) {
 
             {/* sắp xếp lương từ cao đến thấp */}
 
+            <button onClick={thapdencao}>Sắp xếp lương từ thấp đến cao</button>
             <button onClick={caodenthap}>Sắp xếp lương từ cao đến thấp</button>
-
 
             <div className='container'>
                 <Breadcrumb>
                     <BreadcrumbItem><Link to="/">Nhân Viên</Link></BreadcrumbItem>
                     <BreadcrumbItem active>Bảng lương</BreadcrumbItem>
                 </Breadcrumb>
-
 
                 <div className='row'>
                     {mang}
